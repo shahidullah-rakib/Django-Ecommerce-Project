5b93063f68fd8501c41183db1a3004341b11fef9
Rakib Shahidullah <57812923+shsidulla@users.noreply.github.com>
2023-03-20 13:40:41 +0600

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,75 @@
+<h1 align="center">DJANGO E-COMMERCE</h1>
+  <p align="center">
+    A basic e-commerce app.
+    <br />
+  </p>
+</p>
+
+### Steps:
+
+1. Initialize the project
+2. Initialize Store App
+3. Add store app reference in the setting.py of main project
+4. Create template files for store app
+5. Create views - Store, Cart, Checkout
+6. Create the urls in urls.py file of Store app
+7. Include store urls in the main project urls.py 
+8. Create a static folder outside main project and Store app
+9. Create two folder for css and images in static directory
+10. Add static folder reference in settings.py within STATICFILES_DIRS list
+11. Then add main.css in the main.html file of store app
+12. Design html files for frontend of store app
+13. Create database models:
+- Customer
+- Product
+- Order
+- OrderItem
+- ShippingModel
+14. Database migration
+15. Register models
+16. Create a super user
+17. Add 6 dummy products from admin panel
+18. Fetch products dynamically from database using store function in views.py
+19. Creating for loop in store.html render products
+20. For showing images of products
+- Install pillow
+- Create imagefield in Prodcut model
+- Migrate database
+- Make MEDIA_DIR as static/images/products
+- Make MEDIA_URL
+- Modify urlpatterns in urls.py of main project
+- Creating a function in product model to return image url
+21. For creating user cart
+- Create dummy user from admin panel
+- Create dummy order and orderitems from admin panel
+- Fetch orders dynamically from database using cart function in views.py
+- Creating for loop in cart.html render orders
+22. To get total price of cart items and number of total items
+- Create function get_total in OrderItem model which return total price
+- Implement that total price for each product in html 
+- Create two functions in Order model to get total number of items and total price
+- Pass order in the context of cart function of views.py
+- Implement data in html
+23. To get total price and number of items follow step 22 and change
+- checkout.html
+- checkout function in views.py
+24. Now to create 'ADD TO CART' functionality
+- Create cart.js file in static js folder
+- Pass data-product and data-action attributes in 'ADD TO CART' button in HTML
+- Now check if user is logged in or not
+- If user is logged in we will send data
+- For sending data got to views.py and create function updateItem
+- Creat pipeline for updateitem and send a dummy message as response
+- Create function in cart.js to post items and get a response 
+- Now create function in main.html within script tag to get CSRF token
+- Now pass that token as headers attribute
+- Now modify updateitem function in view.py to add or remove products
+25. Now to render number of cart items
+- Modify all the view functions in views.py and pass cartItems to html file
+- Place cartItems at particular position
+26. Now to update the number of items from cart page on clicking arrow keys
+- Add class name 'update-cart' to the up/down arrow buttons
+- Pass attribute data and data-action
+27. Now make payment option visible on submitting the form on checkout page
+28. Make shipping details form interactive
+29. To make an order create a function in view.py and pass data to database
---
+++ ecommerce/store/views.py
@@ -0,0 +1,117 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import *
+import json
+import datetime
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+
+# Store function for rendering store view
+
+def store(req):
+    if req.user.is_authenticated:
+        customer = req.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = [] # For non-authenticated users
+        order={'get_cart_total': 0, 'get_cart_items': 0} # For non-authenticated users
+        cartItems = order['get_cart_items']
+
+    products = Product.objects.all()
+    context = {'products': products, 'cartItems': cartItems}
+    return render(req, 'store/store.html', context)
+
+
+# Cart function for rendering cart view
+
+def cart(req):
+    if req.user.is_authenticated:
+        customer = req.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        print(order)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = [] # For non-authenticated users
+        order={'get_cart_total': 0, 'get_cart_items': 0} # For non-authenticated users
+        cartItems = order['get_cart_items']
+
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
+    return render(req, 'store/cart.html', context)
+
+
+# Checkout function for rendering checkout view
+
+def checkout(req):
+    if req.user.is_authenticated:
+        email = req.user.email
+        customer = req.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        print(order)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = [] # For non-authenticated users
+        order={'get_cart_total': 0, 'get_cart_items': 0} # For non-authenticated users
+        cartItems = order['get_cart_items']
+
+    context = {'items': items, 'order': order, 'cartItems': cartItems, 'email': email}
+    return render(req, 'store/checkout.html', context)
+
+
+# Update cart items
+
+def updateItem(req):
+    data = json.loads(req.body)
+    productId = data['productId']
+    action = data['action']
+
+    customer = req.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+
+    if action == 'add':
+        print("Added")
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        print("Removed")
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added.', safe=False)
+
+def processOrder(req):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(req.body)
+    if req.user.is_authenticated:
+        customer = req.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        ShippingModel.objects.create(
+            customer = customer,
+            order = order,
+            address = data['shipping']['address'],
+            city = data['shipping']['city'],
+            state = data['shipping']['state'],
+            zipcode = data['shipping']['zipcode'],
+        )
+    else:
+        print('User is not logged in.')
+
+    return JsonResponse('Payment complete.', safe=False)
